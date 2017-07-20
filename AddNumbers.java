488fd437e67dd45521d18076185e241c7109a134
J Surender <surenderj@HCL.COM>
2017-07-20 16:19:57 +0530

    changenum

--- AddNumbers.java
+++ AddNumbers.java
@@ -4,8 +4,8 @@ public class AddNumbers {
 
 	public static void main(String[] args) {
 		
-	int number1=8;;
-	int number2=10;
+	int number1=15;;
+	int number2=20;
 	System.out.println(number1+number2);
 	}
 
